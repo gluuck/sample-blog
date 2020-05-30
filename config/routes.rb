d896d7da7357e67c4177a4d0397b4a9577fa3805
Vladimir Ostapenko <ostapenkov74@gmail.com>
2020-05-30 18:40:20 +0500

    change /contacts/new to /contacts

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
   get 'home/index'
  
   root 'home#index'
-  resource :contacts , only:[:new , :create]
+  resource :contacts , only:[:new , :create] , path_names: {:new=>''}
   resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
