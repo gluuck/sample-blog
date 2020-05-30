e4e9191f8e9c14b7979736f38ac9c6c0d0953861
Vladimir Ostapenko <ostapenkov74@gmail.com>
2020-05-30 18:03:51 +0500

    save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,12 @@ class ContactsController < ApplicationController
   def new
   end
   def create
+    @contact = Contact.new(contact_params)
+    @contact.save
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:email, :message)
   end
 end
