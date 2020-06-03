48c5d497bd211ff4aab5e38cad38e182c7721771
gluuck <ostapenkov74@gmail.com>
2020-06-03 22:46:49 +0500

    change contat db

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_05_30_114349) do
+ActiveRecord::Schema.define(version: 2020_06_03_170920) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
